35a87c3dad32d7ac17aa2a104cc69a7b749ba1c6
R.Klifeld <klifeldr@gmail.com>
2020-12-03 21:13:29 +0100

    Added home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          ClipPath(
+            child: Container(
+              width: _width,
+              height: _height,
+              color: Colors.black,
+            ),
+            clipper: TopTriangle(),
+          ),
+          ClipPath(
+            child: Container(
+              width: _width,
+              height: _height,
+              color: Colors.white12,
+            ),
+            clipper: BottomTriangle(),
+          ),
+          Column(
+            children: [
+              new Expanded(
+                flex: 3,
+                child: Center(
+                  child: new Container(
+                    color: Colors.amber,
+                    width: 250,
+                    height: 200,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      child: RaisedButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        padding: EdgeInsets.all(0.0),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.red, Colors.black],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              transform: GradientRotation(math.pi / 4),
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Container(
+                            constraints: BoxConstraints(
+                                maxWidth: 300.0, minHeight: 50.0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              "PLAY",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50.0,
+                      margin: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        padding: EdgeInsets.all(0.0),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.white, Colors.black],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              transform: GradientRotation(math.pi / 4),
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Container(
+                            constraints: BoxConstraints(
+                                maxWidth: 300.0, minHeight: 50.0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              "CHAT",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TopTriangle extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class BottomTriangle extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width, 0.0);
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
