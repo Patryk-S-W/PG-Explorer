5af3c87c4e6a88bbaba3346e6b12b44ba9a5e309
Jan Zarzycki <jzarzycki97@gmail.com>
2021-01-30 13:27:59 +0100

    Added validation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
-// const String SERVER_URL = 'https://socket--node-chat.herokuapp.com/';
-const String SERVER_URL = 'http://10.0.2.2:3000';
+const String SERVER_URL = 'socket--node-chat.herokuapp.com';
+const String SERVER_URL_HTTPS = 'https://socket--node-chat.herokuapp.com';
+// const String SERVER_URL = 'http://10.0.2.2:3000';
 // const String SERVER_URL = 'http://192.168.0.81:3000';
 
 const String SERVER_NAMESPACE = '/';
--- lib/pages/login_chat_page.dart
+++ lib/pages/login_chat_page.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/constants.dart';
 import 'package:flutter_chat/router/router.gr.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 
 class LoginChatPage extends StatefulWidget {
   LoginChatPage({Key key}) : super(key: key);
@@ -12,6 +17,7 @@ class LoginChatPage extends StatefulWidget {
 
 class _LoginChatPageState extends State<LoginChatPage> {
   TextEditingController _usernameController;
+  bool _validate = true;
 
   @override
   void initState() {
@@ -21,7 +27,7 @@ class _LoginChatPageState extends State<LoginChatPage> {
 
   @override
   void dispose() {
-    _usernameController.clear();
+    _usernameController.dispose();
     super.dispose();
   }
 
@@ -46,78 +52,112 @@ class _LoginChatPageState extends State<LoginChatPage> {
           ],
         ),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(horizontal: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 3,
-              child: Center(
-                child: Text('LeChat',
-                    style: GoogleFonts.rockSalt(
-                      textStyle: TextStyle(
-                        color: Colors.white,
-                        fontSize: 55,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                    controller: _usernameController,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 3,
+                child: Center(
+                  child: Text('LeChat',
+                      style: GoogleFonts.rockSalt(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 55,
+                          fontWeight: FontWeight.bold,
                         ),
+                      )),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      controller: _usernameController,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                        hintText: 'username',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        errorText: _validate ? null : 'Enter username',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5.0),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: EdgeInsets.all(20.0),
                       ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: EdgeInsets.all(20.0),
                     ),
-                  ),
-                  SizedBox(
-                    height: 35.0,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: OutlineButton(
-                        borderSide: BorderSide(color: Colors.white),
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(fontWeight: FontWeight.w400),
-                        ),
-                        onPressed: () {
-                          _openChat();
-                        }),
-                  ),
-                ],
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: OutlineButton(
+                          borderSide: BorderSide(color: Colors.white),
+                          child: Text(
+                            'JOIN',
+                            style: TextStyle(fontWeight: FontWeight.w400),
+                          ),
+                          onPressed: () {
+                            _openChat();
+                          }),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
+  _validation() {
+    setState(() {
+      _usernameController.text.isEmpty ? _validate = false : _validate = true;
+    });
+    return _validate;
+  }
+
   _openChat() async {
-    Future.delayed(
-        Duration(milliseconds: 5),
-        () => {
-              ExtendedNavigator.root.push(Routes.chatPage,
-                  arguments:
-                      ChatPageArguments(username: _usernameController.text)),
-              _usernameController.clear()
-            });
+    FocusScope.of(context).unfocus();
+
+    if (_validation()) {
+      try {
+        await InternetAddress.lookup(SERVER_URL);
+      } on SocketException catch (err) {
+        print('==> $err');
+        showTopSnackBar(
+          context,
+          Padding(
+              padding: EdgeInsets.only(top: 55.0),
+              child: CustomSnackBar.error(
+                message: "No internet connectivity",
+              )),
+        );
+        return;
+      }
+
+      Future.delayed(
+          Duration(milliseconds: 5),
+          () => {
+                ExtendedNavigator.root.push(Routes.chatPage,
+                    arguments:
+                        ChatPageArguments(username: _usernameController.text)),
+                _usernameController.clear()
+              });
+    }
   }
 }
